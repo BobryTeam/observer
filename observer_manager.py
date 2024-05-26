d90a5cfab682b408ee7afc4cbf5e6a5e8380fe43
vitosotdihaet <vitostikitos@gmail.com>
2024-05-26 21:03:55 +0300

    Remake the main loop

--- observer_manager.py
+++ observer_manager.py
@@ -2,58 +2,43 @@ from typing import Dict
 from queue import Queue
 
 from threading import Thread
-from threading import Timer
+
+import time
 
 from events.event import *
 from events.kafka_event import *
 
 from trend_data.trend_data import TrendData
-from scale_data.scale_data import ScaleData
 
 from microservice.microservice import Microservice
+
 
 class ObserverManager(Microservice):
     '''
     Класс отвечающий за представление Observer manager
     Его задача -- отправлять запрос на проведение анализа, пересылать полученные данные Desicion module, отправлять запрос на сбор метрик
     '''
-    TIMER_SEND_METRIC_COLLECT_EVENT = 300.0
-    TIMER_SEND_ANALYSE_TREND_EVENT = 60.0
+    
+    TIMER_SEND_GET_METRICS_EVENT = 300.0
+
 
     def __init__(self, event_queue: Queue, writers: Dict[str, KafkaEventWriter]):
         '''
         Инициализация класса:
-         - `self.timer_get_metrics  - таймер для отправки ивента на сбор метрик`
-         - `self.timer_analyse - таймер для отправки ивента на проведение анализа`
+        `self.main_thread` - основной поток с главным лупом проекта
         '''
-        self.timer_get_metrics = None
-        self.timer_analyse = None
-        return super().__init__(event_queue, writers)
+        super().__init__(event_queue, writers)
 
+        self.main_thread = Thread(target=self.send_get_metrics_event)
+        self.main_thread.start()
 
-    def start_timer(self):
-        '''
-        Запуск таймера для отправки ивентов
-        '''
-        self.timer_get_metrics = Timer(self.TIMER_SEND_METRIC_COLLECT_EVENT, self.send_get_metrics_event)
-        self.timer_get_metrics.start()
-        
     def send_get_metrics_event(self):
         '''
         Отправка ивента на сбор метрик
         '''
-        self.writers['mtrc'].send_event(Event(EventType.GetMetrics, ''))
-        self.timer_get_metrics = None
-        self.timer_analyse = Timer(self.TIMER_SEND_ANALYSE_TREND_EVENT, self.send_analyse_trend_event)
-        self.timer_analyse.start()
-
-    def send_analyse_trend_event(self):
-        '''
-        Отправка ивента на проведение анализа
-        '''
-        self.writers['tran'].send_event(Event(EventType.AnalyseTrend, ''))
-        self.timer_analyse=None
-        self.start_timer
+        while True:
+            time.sleep(self.TIMER_SEND_GET_METRICS_EVENT)
+            self.writers['mtrc'].send_event(Event(EventType.GetMetrics, ''))
 
     def handle_event(self, event: Event):
         '''
@@ -62,6 +47,8 @@ class ObserverManager(Microservice):
         target_function = None
 
         match event.type:
+            case EventType.GotMetrics:
+                target_function = self.handle_event_got_metrics
             case EventType.TrendData:
                 target_function = self.handle_event_trend_data
             case _:
@@ -70,6 +57,9 @@ class ObserverManager(Microservice):
         if target_function is not None:
             Thread(target=target_function, args=(event.data,)).start()
 
-    def handle_event_trend_data(self, scale_data: ScaleData):
-        # send trend data to DM Manager
-        self.writers['dmm'].send_event(Event(EventType.TrendData, scale_data))
+    def handle_event_got_metrics(self, _):
+        self.writers['tran'].send_event(Event(EventType.AnalyseTrend, ''))
+
+    def handle_event_trend_data(self, trend_data: TrendData):
+        # send TrendData to DM Manager
+        self.writers['dmm'].send_event(Event(EventType.TrendData, trend_data))
